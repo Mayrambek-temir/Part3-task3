8ad682748854a11ed2d5bd985f587f129e9d8077
Mayrambek-temir <m.temirov2015@gmail.com>
2019-11-17 09:29:08 +0300

    task3

---
+++ Task3.py
@@ -0,0 +1,9 @@
+N = int(input('Enter number of students: '))
+
+Num1=(N-1)
+Num2=(N+1)
+Res1=str(Num1)
+Res2=str(Num2)
+Num3=str(N)
+print('The next number for the number: '+ Num3 + ' is  '+Res2)
+print('he previous number for the: '+ Num3 + ' is  '+Res1)
